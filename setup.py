80ca710cd43a5fa62318f31d59badbd9e23f1cc5
jimherefornonsense <jim.zijingzhou.tw@gmail.com>
2023-04-14 21:24:14 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mini_chess',
+    version='0.1',
+    description='A package for playing mini chess',
+    author='jimherefornonsense',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
